588341a673764edd0d42c90b3fd6e3e7c8721b0a
Dilshaad21 <dilshaadmuthalif21@gmail.com>
2020-10-12 12:37:54 +0530

    Added input check

--- proj4/sqrt.c
+++ proj4/sqrt.c
@@ -12,10 +12,23 @@ int main(int argc, char* argv[]) {
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
-
-	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
-	printf("End of program. Exiting.");
+	char *ch = argv[1];
+	int flag = 1;
+	while(*ch!='\0'){
+		if(!(*ch>=48 && *ch<=57)){
+			flag = -1;
+			break;
+		}	
+		ch++;		
+	}
+	if(flag==1){
+		int input = atoi(argv[1]);
+		printf("Sqrt of %d is %f\n",input,sqrt(input));
+	}
+	else{
+		printf("Input is an invalid number\n");	
+	}
+	printf("End of program. Exiting.\n");
 	return(0);
 
 } // end main
